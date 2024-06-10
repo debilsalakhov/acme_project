707d9af2f335593bdb5568bb121ee485b2656f33
Shamil Salakhov <debilsalakhov@yandex.ru>
2024-06-10 12:41:07 +0300

    sprint 10

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+from django.urls import reverse
+
+from .validators import real_age
+
+
+class Birthday(models.Model):
+    first_name = models.CharField('Имя', max_length=20)
+    last_name = models.CharField(
+        'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
+    )
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', blank=True, upload_to='birthdays_images')
+
+    class Meta:
+        verbose_name = 'день рождения'
+        verbose_name_plural = 'Дни рождения'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
+
+    def get_absolute_url(self):
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
